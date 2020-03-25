1b87beec53a2ef021976100511343932dceade92
pseudo <sybsss@sina.cn>
2020-03-25 12:04:31 +0800

    发布到npm-v2

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import '@/styles/index.scss'; // global css
 import 'normalize.css';
-import designRuler from 'design-ruler';
+import designRuler from '../plugins';
 Vue.use(designRuler);
 
 Vue.config.productionTip = false;
